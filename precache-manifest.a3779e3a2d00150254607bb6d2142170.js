46f5d863770b7ed0508260909c630b56ade56e7f
Debanjan Ghosh <debanjang@ymail.com>
2020-01-19 17:21:10 +0530

    Updates

---
+++ precache-manifest.a3779e3a2d00150254607bb6d2142170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c112ba3d6bcec72041b636c8f3965862",
+    "url": "/react-clock/index.html"
+  },
+  {
+    "revision": "bd6e4dfb7eb1c19186b2",
+    "url": "/react-clock/static/css/main.1b17962a.chunk.css"
+  },
+  {
+    "revision": "beb12c4f898414d1fe4c",
+    "url": "/react-clock/static/js/2.3e5dea9d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-clock/static/js/2.3e5dea9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd6e4dfb7eb1c19186b2",
+    "url": "/react-clock/static/js/main.b8cfc784.chunk.js"
+  },
+  {
+    "revision": "c42b6b6b6d330ef1f3cf",
+    "url": "/react-clock/static/js/runtime-main.ce520e0e.js"
+  }
+]);
